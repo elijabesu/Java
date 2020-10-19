66b5660589f95cbe569839a7c4e59796cb5798f4
elijabesu <ellie@saurich.com>
2020-10-19 13:47:27 +0200

    Part 9 done!

--- mooc_fi-ii/src/parts/nine/Main.java
+++ mooc_fi-ii/src/parts/nine/Main.java
@@ -3,9 +3,10 @@ package parts.nine;
 public class Main {
     public static void main(String[] args) {
             // 1. CLASS INHERITANCE //
-        //parts.nine.inheritance.Main.main(args);
+        parts.nine.inheritance.Main.main(args);
             // 2. INTERFACES //
         parts.nine.interfaces.Main.main(args);
             // 3. OBJECT POLYMORPHISM //
+        parts.nine.polymorphism.Main.main(args);
     }
 }
