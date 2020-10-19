0079fd4db1352ead263770714f399ef41c2ff325
elijabesu <ellie@saurich.com>
2020-10-19 15:12:33 +0200

    Preparing for part 10

---
+++ mooc_fi-ii/src/parts/ten/Main.java
@@ -0,0 +1,12 @@
+package parts.ten;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. HANDLING COLLECTIONS AS STREAMS //
+        parts.ten.streams.Main.main(args);
+        // 2. THE COMPARABLE INTERFACE //
+        //parts.ten.comparable.Main.main(args);
+        // 3. OTHER USEFUL TECHNIQUES //
+        //parts.ten.techniques.Main.main(args);
+    }
+}
