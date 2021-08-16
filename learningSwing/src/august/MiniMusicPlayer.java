9b73d44ed3006080eb2b87532b3a8ce231baf377
elijabesu <41390133+elijabesu@users.noreply.github.com>
2021-08-16 15:40:23 +0200

    MusicPlayer pt. 2

--- learningSwing/src/august/MiniMusicPlayer.java
+++ learningSwing/src/august/MiniMusicPlayer.java
@@ -2,18 +2,32 @@ package august;
 
 import javax.sound.midi.*;
 
-public class MiniMusicPlayer {
+public class MiniMusicPlayer implements ControllerEventListener {
 
     public static void main(String[] args) {
+        MiniMusicPlayer miniMusicPlayer = new MiniMusicPlayer();
+        miniMusicPlayer.go();
+    }
+
+    public void go() {
         try {
             Sequencer sequencer = MidiSystem.getSequencer();
             sequencer.open();
+
+            int[] eventsIWant = {127}; // want even #127
+            sequencer.addControllerEventListener(this, eventsIWant);
 
             Sequence sequence = new Sequence(Sequence.PPQ, 4);
             Track track = sequence.createTrack();
 
             for (int i = 5; i < 64; i += 4) {
                 track.add(makeEvent(144, 1, i, 100, i));
+
+                track.add(makeEvent(176, 1, 127, 0, i));
+                // Inserting own ControllerEvent (176 says the event type is ControllerEvent), looking out for event
+                // 127. This does nothing but is needed so we can get an event. Same tick as event NOTE ON which is
+                // what we needed.
+
                 track.add(makeEvent(128, 1, i, 100, i + 2));
             }
 
@@ -23,6 +37,11 @@ public class MiniMusicPlayer {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void controlChange(ShortMessage event) {
+        System.out.println("la");
     }
 
     public static MidiEvent makeEvent(int comd, int channel, int one, int two, int tick) {
