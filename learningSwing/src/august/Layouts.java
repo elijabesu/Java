b985d8147397ddc518b12eccf2488d5dfa8f104e
elijabesu <41390133+elijabesu@users.noreply.github.com>
2021-08-16 16:38:38 +0200

    Layouts pt. 2 - FlowLayout

--- learningSwing/src/august/Layouts.java
+++ learningSwing/src/august/Layouts.java
@@ -12,15 +12,20 @@ public class Layouts {
 
     public void go() {
         JFrame frame = new JFrame(); // JFrame's default is BorderLayout
-        JButton button = new JButton("click me");
-//        JButton button = new JButton("click me like you mean it");
-        Font bigFont = new Font("QuickSand", Font.BOLD, 28);
+        JPanel panel = new JPanel(); // JPanel's default is FlowLayout
+        panel.setBackground(Color.PINK);
+//        JButton button = new JButton("click me");
+        JButton button = new JButton("click me like you mean it");
+        Font bigFont = new Font("QuickSand", Font.PLAIN, 28);
         button.setFont(bigFont);
         // buttons get as wide as they want to be when placed EAST or WEST
         // buttons get as tall as they want to be when placed NORTH or SOUTH
         // components in the CENTER get whatever space is left
+        panel.add(new JButton("click me"));
+        panel.add(new JButton("touch me"));
         frame.getContentPane().add(BorderLayout.NORTH, button);
-        frame.setSize(200, 200);
+        frame.getContentPane().add(BorderLayout.EAST, panel);
+        frame.setSize(500, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
